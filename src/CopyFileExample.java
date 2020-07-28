2ff91bef62c4277fce648562f6fb395906e1aff2
admin <daominhhiep97@gmail.com>
2020-07-28 11:22:15 +0700

    [Optional] [Bài tập] Copy file nhị phân

---
+++ src/CopyFileExample.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class CopyFileExample {
+    public static void main(String[] args) throws IOException {
+        InputStream inStream = null;
+        OutputStream outStream = null;
+
+        try {
+            inStream = new FileInputStream(new File("./txt/file1.txt"));
+            outStream = new FileOutputStream(new File("./txt/file2.txt"));
+            int count = 0;
+            int length;
+            byte[] buffer = new byte[1024];
+
+            while ((length = inStream.read(buffer)) > 0) {
+                outStream.write(buffer, 0, length);
+                count+=length;
+            }
+            System.out.println("File is copied successful!" );
+            System.out.println("Number of characters in file : " + count);
+        } catch (IOException e) {
+            System.err.println("Can't find file1.txt");
+        } finally {
+            inStream.close();
+            outStream.close();
+        }
+    }
+}
